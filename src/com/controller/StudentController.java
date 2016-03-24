6369bcd61edd9e2c378591f30a7c72d63cea9a89
yuanguangxin <274841922@qq.com>
2016-03-24 20:33:43 +0800

    add student

--- src/com/controller/StudentController.java
+++ src/com/controller/StudentController.java
@@ -1,7 +1,15 @@
 package com.controller;
 
+import com.models.Student;
 import com.service.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class StudentController {
@@ -13,5 +21,60 @@ public class StudentController {
 
     public void setStudentService(StudentService studentService) {
         this.studentService = studentService;
+    }
+
+    @RequestMapping("/login")
+    public void login(@RequestParam(value = "id") int id,
+                      @RequestParam(value = "password") String password,
+                      HttpServletResponse response,
+                      HttpSession session) throws Exception{
+        Student student = studentService.login(id,password);
+        if(student==null){
+            response.getWriter().print("wrong");
+        }else{
+            session.setAttribute("student",student);
+        }
+    }
+
+    @RequestMapping("/addStudent")
+    public String addStudent(Student student){
+        studentService.addStudent(student);
+        return "test.jsp";
+    }
+
+    @RequestMapping("/deleteStudent")
+    public String deleteStudent(@RequestParam(value = "id") int id){
+        studentService.deleteStudent(id);
+        return "test.jsp";
+    }
+
+    @RequestMapping("/modifyStudent")
+    public String modifyStudent(@RequestParam(value = "id") int id,
+                                HttpServletRequest request){
+        Student student = studentService.modify(id);
+        request.setAttribute("modify",student);
+        return "test.jsp";
+    }
+
+    @RequestMapping("/updateStudent")
+    public String updateStudent(Student student){
+        studentService.update(student);
+        return "test.jsp";
+    }
+
+    @RequestMapping("/findByName")
+    public String findByName(@RequestParam(value = "name") String name,
+                             HttpServletRequest request){
+        List<Student> list = studentService.findByName(name);
+        request.setAttribute("students",list);
+        return "test.jsp";
+    }
+
+    @RequestMapping("/findById")
+    public String findById(@RequestParam(value = "id") int id,
+                           HttpServletRequest request){
+        Student student = studentService.findById(id);
+        request.setAttribute("student",student);
+        return "test.jsp";
     }
 }
--- src/com/service/StudentService.java
+++ src/com/service/StudentService.java
@@ -1,6 +1,10 @@
 package com.service;
 
 import com.mapper.StudentMapper;
+import com.models.Student;
+import com.models.StudentExample;
+
+import java.util.List;
 
 public class StudentService {
     private StudentMapper studentMapper;
@@ -11,5 +15,47 @@ public class StudentService {
 
     public void setStudentMapper(StudentMapper studentMapper) {
         this.studentMapper = studentMapper;
+    }
+
+    public Student login(int id, String password) {
+        StudentExample studentExample = new StudentExample();
+        StudentExample.Criteria criteria = studentExample.createCriteria();
+        criteria.andIdEqualTo(id).andPasswordEqualTo(password);
+        List<Student> list = studentMapper.selectByExample(studentExample);
+        if (list.size() == 0) return null;
+        else return list.get(0);
+    }
+
+    public void addStudent(Student student) {
+        studentMapper.insert(student);
+    }
+
+    public void deleteStudent(int id) {
+        StudentExample studentExample = new StudentExample();
+        StudentExample.Criteria criteria = studentExample.createCriteria();
+        criteria.andIdEqualTo(id);
+        studentMapper.deleteByExample(studentExample);
+    }
+
+    public Student modify(int id){
+        Student student = studentMapper.selectByPrimaryKey(id);
+        return student;
+    }
+
+    public void update(Student student) {
+        studentMapper.updateByPrimaryKeySelective(student);
+    }
+
+    public List<Student> findByName(String name) {
+        StudentExample studentExample = new StudentExample();
+        StudentExample.Criteria criteria = studentExample.createCriteria();
+        criteria.andNameLike(name);
+        List<Student> list = studentMapper.selectByExample(studentExample);
+        return list;
+    }
+
+    public Student findById(int id) {
+        Student student = studentMapper.selectByPrimaryKey(id);
+        return student;
     }
 }
